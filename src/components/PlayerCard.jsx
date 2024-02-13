b70486ba3e13b26c518db6ecf29ae27f3d52a94c
Abhishek Hegde <ahegde2021@gmail.com>
2024-02-13 23:12:46 +0530

    added shadcn

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -7,21 +7,35 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
+
 function PlayerCard({ title, description, sources }) {
   return (
-    <div className="bg-blue-500 flex flex-col">
-      <Card className="bg-red-500 flex flex-col">
-        <CardHeader>
-          <CardTitle className="text-bold">{title}</CardTitle>
-        </CardHeader>
-        <video controls>
-          <source src={sources[0]} type="video/mp4" />
-        </video>
-        <CardContent className="flex-grow">
-          <CardDescription>{description}</CardDescription>
-        </CardContent>
-      </Card>
-    </div>
+    <HoverCard>
+      <div className="bg-purple-500">
+        <HoverCardTrigger>
+          <div className="bg-blue-500 flex flex-col">
+            <Card className="bg-red-500 flex flex-col">
+              <CardHeader>
+                <CardTitle className="text-bold">{title}</CardTitle>
+              </CardHeader>
+              <video controls>
+                <source src={sources[0]} type="video/mp4" />
+              </video>
+            </Card>
+          </div>
+        </HoverCardTrigger>
+        <HoverCardContent>
+          <CardContent className="flex-grow">
+            <CardDescription>{description}</CardDescription>
+          </CardContent>
+        </HoverCardContent>
+      </div>
+    </HoverCard>
   );
 }
 export default PlayerCard;

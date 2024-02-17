a72a527cbe53b6d05c3f60a1a3db61ed7b00f92c
Abhishek Hegde <ahegde2021@gmail.com>
2024-02-18 00:07:41 +0530

    Playlist style added

--- src/components/pages/Video.jsx
+++ src/components/pages/Video.jsx
@@ -10,22 +10,25 @@ export default function Video(props) {
   const { title, description, src } = useSelector((state) => state.video);
 
   return (
-    <div className="flex w-full h-screen justify-between bg-gray-800 text-white transition-all duration-500 ease-in-out overflow-hidden">
-      <div className="flex-grow p-8 transform hover:scale-105 transition-transform duration-300 ease-in-out overflow-y-auto">
-        <Card className="bg-gray-900 rounded-lg shadow-lg flex flex-col justify-center transition-all duration-500 ease-in-out hover:shadow-2xl">
-          <CardTitle className="flex justify-center p-4 text-4xl font-bold mb-4 transition-all duration-500 ease-in-out hover:text-blue-500">
-            <h1 className="text-center">{title}</h1>
+    <div className="grid pt-4 grid-cols-4 items-center text-center gap-4 h-screen bg-black overflow-clip">
+      <div className="col-span-3 h-full">
+        <Card className="bg-gray-900 p-8 rounded-lg shadow-lg flex flex-col justify-center transition-all duration-500 ease-in-out hover:shadow-2xl">
+          <CardTitle className="text-4xl font-bold mb-4 transition-all duration-500 ease-in-out hover:text-blue-500">
+            <h1 className="text-center p-8 text-3xl">{title}</h1>
           </CardTitle>
           <CardContent className="mb-4 transition-all duration-500 ease-in-out hover:bg-gray-800">
-            <Player key={src} src={src} />
+            <Player
+              key={src}
+              src={src}
+              className=" cursor-pointer bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 hover:scale-105 transition-all duration-500 ease-in-out"
+            />
           </CardContent>
-          <CardDescription className="p-4 justify-evenly text-lg transition-all duration-500 ease-in-out hover:text-blue-500">
-            <p className="text-center">{description}</p>
+          <CardDescription className="text-lg transition-all p-8 duration-500 ease-in-out hover:text-blue-500">
+            <p className="text-2xl p-8">{description}</p>
           </CardDescription>
         </Card>
       </div>
-
-      <div className="w-1/4 bg-gray-700 p-4 transform hover:scale-105 transition-transform duration-300 ease-in-out overflow-y-auto">
+      <div className="col-span-1 h-full">
         <PlayList />
       </div>
     </div>
